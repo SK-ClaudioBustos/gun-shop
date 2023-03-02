bda8d7eb41cf87b3ed43fc47bd22e7e360a0458c
Claudio Bustos <75331713+SK-ClaudioBustos@users.noreply.github.com>
2023-03-02 12:57:49 -0300

    add : imagen de muestra modify : se agrego la ruta de la imagen de muestra en GalleryImage.jsx modify : estilo imagen de GalleryImage

--- src/components/GalleryImage.jsx
+++ src/components/GalleryImage.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import "../sass/GalleryImage.scss";
 import Image from 'react-bootstrap/Image'
-
+import source from "../img/arma.jpg";
 function GalleryImage(){
     return(
         <div className="gallery-div col-12 col-md-6 col-lg-4 d-flex justify-content-center align-items-center">
-            <Image className="gallery-img rounded" src="https://media.geeksforgeeks.org/wp-content/uploads/20210425000233/test-300x297.png"/>                
+            <Image className="gallery-img rounded" src={source} alt=".."/>                
         </div>
     );
 }
