1580a3438c2a4c1b162aa37d299637a631411cff
Claudio Bustos <75331713+SK-ClaudioBustos@users.noreply.github.com>
2023-03-07 11:11:00 -0300

    modified:   src/App.js 	modified:   src/components/NavB.jsx 	modified:   src/sass/NavB.scss     add : modal de registro al boton reg del navbar     add : modal de inicio de sesion al boton de ing del navbar

--- src/App.js
+++ src/App.js
@@ -10,9 +10,9 @@ function App() {
     <div className="App">
       <NavB></NavB>
       <Gallery id="seccion1" section_name="Rifles" categoria="rifles"/>
-      <Gallery id="seccion2" section_name="Escopetas" categoria="escopetas"/>
+      {/* <Gallery id="seccion2" section_name="Escopetas" categoria="escopetas"/>
       <Gallery id="seccion3" section_name="Pistolas" categoria="pistolas"/>
-      <Gallery id="seccion4" section_name="Subfusiles" categoria="subfusiles"/>
+      <Gallery id="seccion4" section_name="Subfusiles" categoria="subfusiles"/> */}
       <Foot/>
     </div>
   );
--- src/components/NavB.jsx
+++ src/components/NavB.jsx
@@ -1,10 +1,17 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import React, { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import Modal from 'react-bootstrap/Modal';
 import "../sass/NavB.scss"
 
 
 function NavBarFixed() {
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
   return (
         <div className='navbar-container'>
             <Navbar className="navbar-f rounded" expand="md">
@@ -18,10 +25,68 @@ function NavBarFixed() {
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="me-auto nav-container-links">
                             <div className='nav-container-ing'>
-                                <Nav.Link id="nav-l-ing" href="#ing">Iniciar Sesión</Nav.Link>
+                                <Nav.Link id="nav-l-ing" onClick={handleShow} href="#ing">Iniciar Sesión</Nav.Link>
+                                {/* Inicio Modal Iniciar Sesión */}
+                                <Modal show={show} onHide={handleClose} backdrop="static" keyboard="false" fullscreen="md-down">
+                                    <Modal.Header id="navbar-modal">
+                                        <Modal.Title>Iniciar Sesión</Modal.Title>
+                                    </Modal.Header>
+                                    <Modal.Body id="navbar-modal">
+                                        <Form>
+                                            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                                                <Form.Label>Dirección de Email</Form.Label>
+                                                <Form.Control type="email" placeholder="nombre@xxx.com" autoFocus/>
+                                            </Form.Group>
+                                            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                                                <Form.Label>Contraseña</Form.Label>
+                                                <Form.Control type="password" placeholder="******" autoFocus/>
+                                            </Form.Group>
+                                        </Form>
+                                    </Modal.Body>
+                                    <Modal.Footer id="navbar-modal">
+                                        <Button variant="secondary" onClick={handleClose}>
+                                                Cancelar
+                                        </Button>
+                                        <Button variant="dark" onClick={handleClose}>
+                                                Ingresar
+                                        </Button>
+                                    </Modal.Footer>
+                                </Modal>
+                                {/* Final Modal Iniciar Sesión */}
                             </div>
                             <div className='nav-container-reg' id="the_chosen_one">
-                                <Nav.Link id="nav-l-reg" href="#reg">Registrarse</Nav.Link>
+                                <Nav.Link id="nav-l-reg" onClick={handleShow} href="#reg">Registrarse</Nav.Link>
+                                {/* Inicio Modal Registrarse */}
+                                <Modal show={show} onHide={handleClose} backdrop="static" keyboard="false" fullscreen="md-down">
+                                    <Modal.Header id="navbar-modal">
+                                        <Modal.Title>Registrarse</Modal.Title>
+                                    </Modal.Header>
+                                    <Modal.Body id="navbar-modal">
+                                        <Form>
+                                            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                                                <Form.Label>Dirección de Email</Form.Label>
+                                                <Form.Control type="email" placeholder="nombre@xxx.com" autoFocus/>
+                                            </Form.Group>
+                                            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                                                <Form.Label>Contraseña</Form.Label>
+                                                <Form.Control type="password" placeholder="******"/>
+                                            </Form.Group>
+                                            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                                                <Form.Label>Repetir contraseña</Form.Label>
+                                                <Form.Control type="password" placeholder="******"/>
+                                            </Form.Group>
+                                        </Form>
+                                    </Modal.Body>
+                                    <Modal.Footer id="navbar-modal">
+                                        <Button variant="secondary" onClick={handleClose}>
+                                                Cancelar
+                                        </Button>
+                                        <Button variant="dark" onClick={handleClose}>
+                                                Registrarse
+                                        </Button>
+                                    </Modal.Footer>
+                                </Modal>
+                                {/* Final Modal Iniciar Sesión */}
                             </div>
                             <Nav.Link id="nav-l-section" href="#seccion1">Rifles</Nav.Link>
                             <Nav.Link id="nav-l-section" href="#seccio2">Escopetas</Nav.Link>
@@ -30,9 +95,9 @@ function NavBarFixed() {
                         </Nav>   
                     </Navbar.Collapse>    
                 </Container>
+                
             </Navbar>
-        </div>
-        
+        </div>   
   );
 }
 
